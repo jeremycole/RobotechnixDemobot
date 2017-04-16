4387a3ded1ba775bfda31e86ea248c1a48f5e2fd
Jeremy Cole <jeremy@jcole.us>
2017-04-16 02:15:20 -0700

    Strip down AutonomousDriveTest

--- src/main/java/org/firstinspires/ftc/teamcode/AutonomousDriveTest.java
+++ src/main/java/org/firstinspires/ftc/teamcode/AutonomousDriveTest.java
@@ -5,21 +5,9 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 @Autonomous(name = "AutonomousDriveTest")
 @SuppressWarnings("unused")
 public class AutonomousDriveTest extends RobotechnixDemobotOpMode {
-    public void curveAndComeBack() {
-        robot.translateDistance(0, 0.4, 30);
-        robot.rotateToHeading(RobotDrivetrain.RotationDirection.RIGHT, 0.4, 90);
-        robot.translateDistance(0, 0.4, 30);
-        robot.rotateToHeading(RobotDrivetrain.RotationDirection.RIGHT, 0.4, 180);
-
-        robot.rotateToHeading(RobotDrivetrain.RotationDirection.LEFT, 0.4, 90);
-        robot.translateDistance(180, 0.4, 30);
-        robot.rotateToHeading(RobotDrivetrain.RotationDirection.LEFT, 0.4, 0);
-        robot.translateDistance(180, 0.4, 30);
-    }
     @Override
     public void robotRun() {
-        robot.translateDistance(0, 1.0, 60);
-        robot.translateDistance(180, 1.0, 60);
+        robot.translateDistance(0, 0.5, 30);
         robot.stop();
     }
 }
